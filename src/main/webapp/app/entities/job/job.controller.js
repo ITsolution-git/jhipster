a3d161dcf0a72a3607800215377997907f06113d
Roy Smith <roy.smith0820@gmail.com>
2017-02-16 08:44:21 -0500

    jobcontroller fix

--- src/main/webapp/app/entities/job/job.controller.js
+++ src/main/webapp/app/entities/job/job.controller.js
@@ -1,12 +1,9 @@
 (function() {
     'use strict';
-
     angular
         .module('isoftnetApp')
         .controller('JobController', JobController);
-
     JobController.$inject = ['$scope', '$state', 'Job', '$timeout'];
-
     function JobController ($scope, $state, Job, $timeout) {
         //  $rootScope.$on('event:social-sign-in-success', function(event, userDetails){
         //     // console.log(userDetails);
@@ -15,17 +12,14 @@
         //     console.log(logoutStatus);
         // });
         // $rootScope.$on('event:contact-created', function(event, contactInfo){
-
         //     function onSaveSuccess (result) {
         //         $rootScope.$broadcast('isoftnetApp:contactCreate', result);
         //     }
-
         //     function onSaveError (err) {
         //         console.log(err);
         //     }
         //     Contact.save(contactInfo, onSaveSuccess, onSaveError);
         // });
-
         // $scope.signoutSocialMedia = function() {
         //     socialLoginService.logout('facebook');
         // };
@@ -41,31 +35,10 @@
         //         });
         // };
         var vm = this;
-        var mock1 = {
-            "id" : 0,
-            "title" : "Django",
-            "profession" : "python",
-            "duration" : "30",
-            "applicantsCount" : 2,
-            "referralFee" : 299,
-            "company" : "SteelPlant",
-            "shortDescription" : "web developer position",
-            "longDescription" : "sb who",
-            "type" : "FULL_TIME",
-            "status" : "OPEN",
-            "workPermit" : "ANY"
-        };
-        vm.mockData = [];
+        
         $timeout(function() {
-
-            for (var i = 0; i < 20; i++) {
-                var temp = {};
-                angular.copy(mock1, temp);
-                temp.id = i+1;
-                vm.mockData.push(temp);
-            }
             $timeout(function(){
-                var table = $('#job-table').DataTable( {
+                vm.table = $('#job-table').DataTable( {
                     "columns": [
                         { "orderable": false },
                         { "orderable": false },
@@ -96,7 +69,7 @@
                     ],
                     "order": [[2, 'asc']]
                 } );
-                function format ( d ) {
+            function format ( d ) {
                 // `d` is the original data object for the row
                     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
                         '<tr>'+
@@ -106,32 +79,61 @@
                     '</table>';
                 }
          
-                // Add event listener for opening and closing details
-                $('#job-table tbody').on('click', 'td.details-control', function () {
-                    var tr = $(this).closest('tr');
-                    var row = table.row( tr );
-             
-                    if ( row.child.isShown() ) {
-                        // This row is already open - close it
-                        row.child.hide();
-                        tr.removeClass('shown');
-                    }
-                    else {
-                        // Open this row
-                        row.child( format( row.data()) ).show();
-                        tr.addClass('shown');
-                    }
-                } );
-
-
+            // Add event listener for opening and closing details
+            $('#job-table tbody').on('click', 'td.details-control', function () {
+                var tr = $(this).closest('tr');
+                var row = vm.table.row( tr );
+         
+                if ( row.child.isShown() ) {
+                    // This row is already open - close it
+                    row.child.hide();
+                    tr.removeClass('shown');
+                }
+                else {
+                    // Open this row
+                    row.child( format( row.data()) ).show();
+                    tr.addClass('shown');
+                }
+            } );
             }, 100);
         }, 1000);
         
-
+        vm.selectIDs = [];
         vm.jobs = [];
-
         loadAll();
-
+        $scope.addCheck = function(id){
+            if( vm.selectIDs.indexOf(id) == -1 )
+                vm.selectIDs.push(id);
+            else
+                vm.selectIDs.splice(vm.selectIDs.indexOf(id), 1);
+        }
+        $scope.addStatus = function(){
+            $state.go("job.addstatus", {
+                selectIDs : vm.selectIDs
+            });
+            // var arr = $("#job-table tr td:nth-child(1) input:checked");
+            // for (var i = 0; i < arr.length; i++) {
+            //     console.log($(arr[i]).parent().parent().children("td:nth-child(3)").val());
+            // }
+        }
+        $scope.closeJobs = function(){
+            $state.go("job.closejobs", {
+                selectIDs : vm.selectIDs
+            });
+            // var arr = $("#job-table tr td:nth-child(1) input:checked");
+            // for (var i = 0; i < arr.length; i++) {
+            //     console.log($(arr[i]).parent().parent().children("td:nth-child(3)").val());
+            // }
+        }
+        $scope.deleteJobs = function(){
+            $state.go("job.deletejobs", {
+                selectIDs : vm.selectIDs
+            });
+            // var arr = $("#job-table tr td:nth-child(1) input:checked");
+            // for (var i = 0; i < arr.length; i++) {
+            //     console.log($(arr[i]).parent().parent().children("td:nth-child(3)").val());
+            // }
+        }
         function loadAll() {
             Job.query(function(result) {
                 vm.jobs = result;
